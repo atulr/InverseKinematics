3b4053daef747b92d99dfc77e9051357aab1aa4d
Atul Rungta <atulrungta@Atuls-MacBook-Pro.local>
2011-10-15 15:54:36 -0600

    somewhat close to the target

--- test.cpp
+++ test.cpp
@@ -15,15 +15,62 @@
 #include <gsl/gsl_matrix_double.h>
 #include <gsl/gsl_blas.h>
 
-const float PI = 3.14159, length_arm_1 = .5f, length_arm_2 = .3f, beta = 0.01f;
-float joint_angle_1 = (float)PI/2.f;
-float joint_angle_2 = (float)PI/9.f;
+float shoulder = 0.f, elbow = 0.f;
+//static int shoulder = 0, elbow = 0;
+const float PI = 3.14159, length_arm_1 = 1.f, length_arm_2 = 1.f, beta = 0.01f;
+//float shoulder = (float)PI/2.f;
+//float elbow = (float)PI/9.f;
+
+
+void init(void)
+{
+  glClearColor (0.0, 0.0, 0.0, 0.0);
+  glShadeModel (GL_FLAT);
+}
+
+void display(void)
+{
+   glClear (GL_COLOR_BUFFER_BIT);
+   glPushMatrix();
+   glTranslatef (-1.0, 0.0, 0.0);
+   glRotatef ((GLfloat) shoulder * 180/PI, 0.0, 0.0, 1.0);
+   glTranslatef (1.0, 0.0, 0.0);
+   glPushMatrix();
+   glScalef (2.0, 0.4, 1.0);
+   glutWireCube (length_arm_1);
+
+	/* flush GL buffers */
+   glPopMatrix();
+
+   glTranslatef (0.5, 0.0, 0.0);
+   glRotatef ((GLfloat) elbow * 180/PI, 0.0, 0.0, 1.0);
+   glTranslatef (1.0, 0.0, 0.0);
+   glPushMatrix();
+   glScalef (2.0, 0.4, 1.0);
+   glutWireCube (length_arm_2);
+   glPopMatrix();
+
+   glPopMatrix();
+   glutSwapBuffers();
+   glFlush();
+}
+
+void reshape (int w, int h)
+{
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   glTranslatef (0.0, 0.0, -5.0);
+}
 
 gsl_matrix* jacobian(gsl_matrix* J) {
-	gsl_matrix_set(J, 0, 0, length_arm_1 * sin(joint_angle_1) * (-1));
-	gsl_matrix_set(J, 0, 1, length_arm_2 * sin(joint_angle_2) * (-1));
-	gsl_matrix_set(J, 1, 0, length_arm_1 * cos(joint_angle_1));
-	gsl_matrix_set(J, 1, 1, length_arm_2 * cos(joint_angle_2));
+	gsl_matrix_set(J, 0, 0, length_arm_1 * sin(shoulder) * (-1));
+	gsl_matrix_set(J, 0, 1, length_arm_2 * sin(elbow) * (-1));
+	gsl_matrix_set(J, 1, 0, length_arm_1 * cos(shoulder));
+	gsl_matrix_set(J, 1, 1, length_arm_2 * cos(elbow));
 	return J;
 }
 
@@ -78,8 +125,8 @@ gsl_matrix* inverse(gsl_matrix* J) {
 gsl_matrix* end_effector() {
 	gsl_matrix* effector;
 	effector = gsl_matrix_alloc(2,1);
-	gsl_matrix_set(effector, 0, 0, length_arm_2 * cos(joint_angle_2));
-	gsl_matrix_set(effector, 1, 0, length_arm_2 * sin(joint_angle_2));
+	gsl_matrix_set(effector, 0, 0, length_arm_1 * cos(shoulder) + length_arm_2 * cos(shoulder - elbow));
+	gsl_matrix_set(effector, 1, 0, length_arm_1 * sin(shoulder) + length_arm_2 * sin(shoulder + elbow));
 	return effector;
 }
 
@@ -92,6 +139,31 @@ gsl_matrix* difference(gsl_matrix* G, gsl_matrix* E) {
 	gsl_matrix_set(new_matrix, 1, 0, (float) beta * gsl_matrix_get(new_matrix, 1, 0));
 	return new_matrix;
 }
+
+void keyboard (unsigned char key, int x, int y)
+{
+//   switch (key) {
+//      case 's':   /*  s key rotates at shoulder  */
+//         shoulder = (shoulder + 5) % 360;
+//         glutPostRedisplay();
+//         break;
+//      case 'S':
+//         shoulder = (shoulder - 5) % 360;
+//         glutPostRedisplay();
+//         break;
+//      case 'e':  /*  e key rotates at elbow  */
+//         elbow = (elbow + 5) % 360;
+//         glutPostRedisplay();
+//         break;
+//      case 'E':
+//         elbow = (elbow - 5) % 360;
+//         glutPostRedisplay();
+//         break;
+//      default:
+//         break;
+//   }
+}
+
 
 void mouse(int key, int tmp, int x, int y) {
 	float x_norm, y_norm;
@@ -122,24 +194,20 @@ void mouse(int key, int tmp, int x, int y) {
 		 y_norm = (float)(250 - y)/ 250;
 		 gsl_matrix_set(G, 0, 0, x_norm);
 		 gsl_matrix_set(G, 1, 0, y_norm);
-//		 printf("x_norm %f ", x_norm);
-//		 printf("y_norm %f \n", y_norm);
+		 printf(" x %f ", x_norm);
+		 printf(" y %f \n", y_norm);
 		 E = end_effector();
-//		 printf("e_x %f ", length_arm_2 * cos(joint_angle_2));
-//		 printf("e_y %f \n", length_arm_2 * sin(joint_angle_2));
+		 printf(" ex %f ", length_arm_2 * cos(elbow));
+		 printf(" ey %f \n", length_arm_2 * sin(elbow));
+
 		 while(true) {
 			 J = jacobian(J);
 			 J_INV = inverse(J);
 			 del_E = difference(G, E); //del x, del y
 			 gsl_blas_dgemm( CblasNoTrans, CblasNoTrans, 1.0, J_INV, del_E, 0.0, del_THETA );
-//			 printf("%f ", gsl_matrix_get(del_THETA, 0, 0));
-//			 printf("%f \n", gsl_matrix_get(del_THETA, 1, 0));
 
-			 joint_angle_1 += gsl_matrix_get(del_THETA, 0, 0);
-			 joint_angle_2 += gsl_matrix_get(del_THETA, 1, 0);
-//			 printf("ja1 %f \n", joint_angle_1);
-//			 printf("ja2 %f \n", joint_angle_2);
-
+			 shoulder += gsl_matrix_get(del_THETA, 0, 0);
+			 elbow += gsl_matrix_get(del_THETA, 1, 0);
 			 E = end_effector();
 			 if (count++ > 10000){
 				 count = 0;
@@ -151,45 +219,19 @@ void mouse(int key, int tmp, int x, int y) {
 	}
 }
 
-void display(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-    glLineWidth(5.0);
-    glColor3f(255,0 , 0);
-	glBegin (GL_LINES);
-   		glVertex2f (0, 0);
-   		glVertex2f (length_arm_1 * cos(joint_angle_1), length_arm_1 * sin(joint_angle_1)); // change
-	glEnd();
-
-	glColor3f(255,0 , 8);
-	glBegin (GL_LINES);
-	   	glVertex2f (length_arm_1 * cos(joint_angle_1), length_arm_1 * sin(joint_angle_1)); //change
-		glVertex2f (length_arm_2 * cos(joint_angle_2), length_arm_2 * sin(joint_angle_2));
-	glEnd();
-	/* flush GL buffers */
-
-	glFlush();
-
-}
 
 int main(int argc, char** argv)
 {
-	/* Initialize mode and open a window in upper left corner of screen */
-	/* Window title is name of program (arg[0]) */
-
-	/* You must call glutInit before any other OpenGL/GLUT calls */
-	glutInit(&argc,argv);
-	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500,500);
-	glutInitWindowPosition(0,0);
-	glutCreateWindow("Atul Rungta - Assignment 1");
-	glutDisplayFunc(display);
-	glutMouseFunc(mouse);
-//	init();
-	glutMainLoop();
-
+   glutInit(&argc, argv);
+   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+   glutInitWindowSize (500, 500);
+   glutInitWindowPosition (100, 100);
+   glutCreateWindow (argv[0]);
+   init ();
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+//   glutKeyboardFunc(keyboard);
+   glutMouseFunc(mouse);
+   glutMainLoop();
+   return 0;
 }
-
-
-
-
-
